2bc96665c8583287857e96aa76b846e2f6793082
Simon Plenderleith <104832360+simonteaching@users.noreply.github.com>
2022-07-06 21:27:54 +0100

    Add validation for request params, query and body

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,27 +1,66 @@
+import S from "fluent-json-schema";
+
+const paramsSchema = S.object().prop("id", S.number().required());
+
+const filterSchema = S.object().prop(
+	"filter",
+	S.string().minLength(1).maxLength(100)
+);
+
+const recipeSchema = S.object()
+	.required(["name", "ingredients", "time", "steps"])
+	.prop("name", S.string().minLength(3).maxLength(100))
+	.prop(
+		"ingredients",
+		S.array().minItems(1).items(S.string().minLength(3).maxLength(100))
+	)
+	.prop("time", S.number().minimum(1).maximum(1000))
+	.prop("steps", S.string().minLength(10).maxLength(10000));
+
 export default async function recipesRoutes(app) {
-	app.get("/", async function (request, reply) {
-		const filter = request.query.filter;
+	app.get(
+		"/",
+		{ schema: { query: filterSchema } },
+		async function (request, reply) {
+			const filter = request.query.filter;
 
-		request.log.info({ filter });
+			request.log.info({ filter });
 
-		reply.send([]);
-	});
+			reply.send([]);
+		}
+	);
 
-	app.get("/:id", async function (request, reply) {
-		reply.send({ id: request.params.id });
-	});
+	app.get(
+		"/:id",
+		{ schema: { params: paramsSchema } },
+		async function (request, reply) {
+			reply.send({ id: request.params.id });
+		}
+	);
 
-	app.post("/", async function (request, reply) {
-		reply.status(201).send(request.body);
-	});
+	app.post(
+		"/",
+		{ schema: { body: recipeSchema } },
+		async function (request, reply) {
+			reply.status(201).send(request.body);
+		}
+	);
 
-	app.put("/:id", async function (request, reply) {
-		reply.send({ id: request.params.id, ...request.body });
-	});
+	app.put(
+		"/:id",
+		{ schema: { params: paramsSchema, body: recipeSchema } },
+		async function (request, reply) {
+			reply.send({ id: request.params.id, ...request.body });
+		}
+	);
 
-	app.delete("/:id", async function (request, reply) {
-		reply.status(204);
-	});
+	app.delete(
+		"/:id",
+		{ schema: { params: paramsSchema } },
+		async function (request, reply) {
+			reply.status(204);
+		}
+	);
 
 	app.addHook("onSend", async function (request, reply) {
 		reply.header("Cache-Control", "no-store");
