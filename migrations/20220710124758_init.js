5b7a92afc2d4b785beee1d7bb82a4860ce62a212
Simon Plenderleith <simon@simonplend.co.uk>
2023-06-29 16:25:35 -0700

    Remove comment

--- migrations/20220710124758_init.js
+++ migrations/20220710124758_init.js
@@ -1,7 +1,6 @@
 export const up = async (knex) => {
 	return knex.schema.createTable("recipes", function (table) {
 		table.uuid('id').primary().notNullable();
-		// .defaultTo(knex.raw('gen_random_uuid()'))
 		table.text("name");
 		table.text("ingredients");
 		table.integer("time");
